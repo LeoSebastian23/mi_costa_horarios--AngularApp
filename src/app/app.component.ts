797b94a622beeb00a84ff40f048c710e001f60fd
Leonardo Sebastian Gauto <112297703+LeoSebastian23@users.noreply.github.com>
2024-07-19 01:11:36 -0300

    update clocks and tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 import { ClockLiveComponent } from './clock-live/clock-live.component';
 import { HorariosComponent } from './horarios/horarios.component';
-
+import { HorariosService } from './Services/horarios.service';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +12,20 @@ import { HorariosComponent } from './horarios/horarios.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  otamendiTimes: string[] = [];
+  miramarTimes: string[] = [];
+
+  constructor(private horariosService: HorariosService) {}
+
+  ngOnInit(): void {
+    this.horariosService.getHorarios().subscribe(data => {
+      // Asumiendo que los datos vienen en un formato que contiene los horarios de Otamendi y Miramar
+      this.otamendiTimes = data.map(h => h.otamendi_sale);
+      this.miramarTimes = data.map(h => h.miramar_sale);
+    });
+  }
+}
+
+
+
